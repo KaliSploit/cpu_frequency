cbe8c22b8d401230a3f773c26784a4361e78877c
GeneralRepublic <57861793+KaliSploit@users.noreply.github.com>
2020-09-29 18:15:23 +0300

    Update cpu_frequency.py

--- cpu_frequency.py
+++ cpu_frequency.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
 import os, time
-i = 0
-def cpu_frequency():
-	while i != 1:
+def cpu_frequency_linux():
+	while True:
 		os.system('clear')
 		os.system('grep MHz /proc/cpuinfo')
 		time.sleep(1)
 		os.system('clear')
-cpu_frequency()
+
+def cpu_information_windows():
+	while True:
+		os.system("cls")
+		os.system("wmic cpu list brief")
+		time.sleep(1)
+def cpu_information_linux():
+	while True:
+		os.system("clear")
+		os.system("lscpu")
+		time.sleep(1)
+systemtype = input('What system to you use? [1] Windows [2] Linux. : )
+if systemtype == 1:
+	cpu_information_windows()
+elif systemtype == 2:
+	chinf = input("What information you want to collect? [1] Infomation about CPU [2] CPU frequency : ")
+		   
